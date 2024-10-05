87e55fe8e9a48ebb2275783ea8040e1264695431
JuNicolle <juliennicolle@hotmail.fr>
2024-10-05 23:01:12 +0200

    biography OK, technos in progress

---
+++ src/Components/TechnoCard.jsx
@@ -0,0 +1,29 @@
+import JavascriptLogo from "../assets/JavascriptLogo.png";
+
+const TechnoCard = () => {
+    
+    
+    
+    
+    return <>
+        <div className="technoCard">
+          <div>
+            <img src={JavascriptLogo} alt="" />
+          </div>
+          <div>
+            <div>
+              <h3>JS</h3>
+            </div>
+            <div>
+              <p>Javascript est mon langage préféré pour le moment.</p>
+            </div>
+          </div>
+
+
+
+
+        </div>
+    </>
+}
+ 
+export default TechnoCard;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,10 @@ import NavbarComponent from "../Components/Navbarcomponent";
 import GitLogo from "../assets/GitHubLogo.png";
 import LinkedinLogo from "../assets/LinkedinLogo.png";
 import MessageLogo from "../assets/message.png";
+import hackerImg from "../assets/hacker.png";
+import ReactLogo from "../assets/ReactLogo.png";
+import TechnoCard from "../Components/TechnoCard";
+import JavascriptLogo from "../assets/JavascriptLogo.png";
 
 
 
@@ -10,71 +14,143 @@ import MessageLogo from "../assets/message.png";
 
 const HomePage = () => {
   return <>
-   <div className="bodyCorp">
-    <header className='cvReact'>
-      <HomeText/>
-      <NavbarComponent/>
-    </header>
-    
-    <div id="header">
+    <div className="bodyCorp">
+      <header className='cvReact'>
+        <HomeText />
+        <NavbarComponent />
+      </header>
+
+      <div id="header">
         <div className="container">
           <div className="title">JULIEN NICOLLE</div>
           <div className="subtitle">Open To Work</div>
         </div>
+      </div>
     </div>
-    </div> 
     <div id="mainHomePage">
-        <div className="leftPart">
-            <div id="networksSection">
-                  <div class="avatar">
-                  <a href="">
-                    <img id="gitImg" src={GitLogo} alt="logo github" />
-                  </a>
-                  </div>
+      <div className="leftPart">
+        <div id="networksSection">
+          <div class="avatar">
+            <a href="https://github.com/JuNicolle">
+              <img id="gitImg" src={GitLogo} alt="logo github" />
+            </a>
+          </div>
 
-                  <div class="avatar">
-                  <a href="">
-                    <img id="linkedinImg" src={LinkedinLogo} alt="logo linkedin" />
-                  </a>
-                  </div>
+          <div class="avatar">
+            <a href="https://www.linkedin.com/in/junicolle/">
+              <img id="linkedinImg" src={LinkedinLogo} alt="logo linkedin" />
+            </a>
+          </div>
 
-                  <div class="avatar">
-                  <a href="">
-                    <img id="linkedinImg" src={MessageLogo} alt="logo linkedin" />
-                  </a>
-                  </div>
-            </div>
-            <div id="technosSection">
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-                    <img  src={MessageLogo} alt="" />
-            </div>
+          <div class="avatar">
+            <a href="tel:0637237419">
+              <img id="messageImg" src={MessageLogo} alt="logo linkedin" />
+            </a>
+          </div>
         </div>
-        <div className="rightPart">
-            <div id="biographySection">
-                <div id="biographyTitleSection">Biographie</div>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, excepturi? Itaque maiores, minima impedit voluptas ipsum ratione harum, aperiam mollitia hic, ipsam voluptate blanditiis repudiandae voluptatibus? Deserunt obcaecati hic repudiandae.
-              Voluptas sint officiis voluptatum! Perferendis unde alias nulla obcaecati mollitia corrupti enim facilis ex, autem illum, atque, ea omnis sequi voluptatem officiis quo et accusamus. Aliquid placeat accusantium praesentium voluptas?
-              Quod autem inventore neque architecto eligendi ullam vel dolores quis quia, nobis debitis ipsam aliquam eos reprehenderit enim voluptatibus provident explicabo magni fuga tenetur officiis iusto repudiandae quas! Dignissimos, nisi?
-              Sint, aliquid? Molestias et natus ipsum similique error veritatis voluptates provident deserunt quod quos corporis, nostrum cupiditate dignissimos animi praesentium esse modi sed. Ab est quibusdam corrupti laboriosam tempora unde.
-              Officiis eos sed aspernatur facere voluptatem nobis dolorem deleniti tempore laudantium labore voluptatum quia qui eum ea quae, sunt, impedit voluptate architecto explicabo, autem quidem. Nostrum libero eligendi temporibus deleniti.
-            </div>
-        </div>
-    </div>    
-        <div>
-          Side Projects
-        </div>
-    
-    
+        <div id="technosSection">
+          <h2>Ce que j'apprends :</h2>
 
-    
-     </>
+
+          <div class="technoCard">
+            <div>
+              <img src={ReactLogo} alt="" />
+            </div>
+            <div>
+              <div>
+                <h3>React</h3>
+              </div>
+              <div>
+                <p>React est un Framework JS.</p>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="technoCard">
+            <div>
+              <img src={ReactLogo} alt="" />
+            </div>
+            <div>
+              <div>
+                <h3>React</h3>
+              </div>
+              <div>
+                <p>React est un Framework JS.</p>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="technoCard">
+            <div>
+              <img src={ReactLogo} alt="" />
+            </div>
+            <div>
+              <div>
+                <h3>React</h3>
+              </div>
+              <div>
+                <p>React est un Framework JS.</p>
+              </div>
+            </div>
+          </div>
+
+        <TechnoCard />
+
+
+
+        </div>
+      </div>
+      <div className="rightPart">
+        <div id="biographySection">
+          <div class="card">
+            <div class="tools">
+              <div class="circle">
+                <span class="red box"></span>
+              </div>
+              <div class="circle">
+                <span class="yellow box"></span>
+              </div>
+              <div class="circle">
+                <span class="green box"></span>
+              </div>
+            </div>
+            <div class="card_content">
+              <img src={hackerImg} id="profilPic" alt="" />
+              <p>
+                Hi, my name is Julien and i'll be a super developper in few months, motherfuckers. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quidem ex eligendi ad praesentium modi explicabo reprehenderit quam perferendis labore, inventore illum ratione tempora suscipit nesciunt corporis nulla qui hic in a doloribus quaerat! Sint, voluptatum reprehenderit, minima maiores eveniet ipsum voluptas delectus enim quaerat soluta repellat earum eius accusantium.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="sidesProjectsSection">
+      Side Projects
+
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+      <TechnoCard></TechnoCard>
+    </div>
+
+
+
+
+  </>
 }
 
 export default HomePage;
